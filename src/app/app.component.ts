036636e2daf6a876c7dd94a4b39e0112f84d6694
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-08 12:44:32 +0300

    Deploying app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+ 
+
+  // myGoals = [];
+  // goals: Goal =
+  //   {id: 1, name: 'Ken'};
+  // allGoals = this.myGoals.push(this.goals);
+
+  // constructor() {
+  //   this.goals = ['Watching Nemo', 'Buy cookies','Get new phone case']
+  // }
 }
